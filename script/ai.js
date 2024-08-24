cab465a9d2b0e2eabdb8dc29cf802e8c79a03c68
Sentinelb <shidorubot@gmail.com>
2024-08-24 23:47:58 +0000

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -4,7 +4,7 @@ module.exports.config = {
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
-  aliases: ['gpt', 'einstein'],
+  aliases: ['gpt', 'openai'],
   description: "An AI command powered by GPT-4",
   usage: "Ai [promot]",
   credits: 'Metoushela Walker  and édit by hamed junior',
@@ -17,17 +17,18 @@ module.exports.run = async function({
 }) {
   const input = args.join(' ');
   if (!input) {
-    api.sendMessage(`[🌐] 𝗘𝗜𝗡𝗦𝗧𝗘𝗜𝗡 𝗕𝗢𝗧 :\n━━━━━━━━━━━━━━━━━━\nHello, How can I be of help to you? \n━━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);
+    api.sendMessage(`[🌐] 𝗖𝗛𝗔𝗧 𝗚𝗣𝗧 💬\n━━━━━━━━━━━━━━━━━━\nHello, How can I be of help to you? \n━━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`[🌐] 𝗘𝗜𝗡𝗦𝗧𝗘𝗜𝗡 𝗕𝗢𝗧 :\n━━━━━━━━━━━━━━━━━━\n "${input}"\n━━━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);
+  api.sendMessage(`𝗚𝗣𝗧 💬\n━━━━━━━━━━━━━━━━━\n "${input}"\n━━━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);
   try {
     const {
       data
     } = await axios.get(`https://metoushela-rest-api-tp5g.onrender.com/api/gpt4o?context=${encodeURIComponent(input)}`);
     const response = data.response;
-    api.sendMessage( '[🌐] 𝗘𝗜𝗡𝗦𝗧𝗘𝗜𝗡 𝗕𝗢𝗧 :\n\n' + response + ⚫'\n\n━━━━━━━━━━━━━━━━━━\n', event.threadID, event.messageID);
+    api.sendMessage( '[🌐] 𝗖𝗛𝗔𝗧 𝗚𝗣𝗧 💬\n━━━━━━━━━━━━━━━━━\n\n' + response + '\n━━━━━━━━━━━━━━━━━━━\n', event.threadID, event.messageID);
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
+    api.setMessageReaction("💬", event.messageID, () => {}, true);
   }
 };
